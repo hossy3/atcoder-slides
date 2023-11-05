fe9d986574ca39e291f658e296cd936458be6bcc
hossy <hossy@hossy.info>
2023-11-06 02:43:31 +0900

    Enable Zoom

--- src/stories/Slide.tsx
+++ src/stories/Slide.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./Slide.css";
 
 interface SlideProps {
@@ -6,5 +7,28 @@ interface SlideProps {
 }
 
 export const Slide = ({ src, label }: SlideProps) => {
-  return <img src={src} aria-label={label} />;
+  const [zoom, setZoom] = useState<0 | 1 | 2>(0);
+  return (
+    <>
+      <img onClick={() => setZoom(1)} src={src} aria-label={label} />
+      {zoom > 0 && (
+        <div onClick={() => setZoom(0)} className="modal-overlay">
+          <div className="modal-content">
+            {zoom == 1 ? (
+              <img
+                src={src}
+                aria-label={label}
+                onClick={(ev) => {
+                  setZoom(2);
+                  ev.stopPropagation();
+                }}
+              />
+            ) : (
+              <img src={src} aria-label={label} className="zoom100" />
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
