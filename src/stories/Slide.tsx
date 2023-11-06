dd678d72f186adfdebf730bcf8aa1db12be824fd
hossy <hossy@hossy.info>
2023-11-07 00:50:06 +0900

    Update Slide.tsx
    Support [Enter] and [Esc] key

--- src/stories/Slide.tsx
+++ src/stories/Slide.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { KeyboardEventHandler, useCallback, useState } from "react";
 import "./Slide.css";
 
 interface SlideProps {
@@ -8,9 +8,27 @@ interface SlideProps {
 
 export const Slide = ({ src, label }: SlideProps) => {
   const [zoom, setZoom] = useState<0 | 1 | 2>(0);
+  const onKeyDown = useCallback<KeyboardEventHandler>(
+    (ev) => {
+      if (ev?.key === "Enter") {
+        setZoom(((zoom + 1) % 3) as 0 | 1 | 2);
+        ev.stopPropagation();
+      } else if (ev?.key === "Escape" && zoom > 0) {
+        setZoom(0);
+        ev.stopPropagation();
+      }
+    },
+    [zoom]
+  );
   return (
     <>
-      <img onClick={() => setZoom(1)} src={src} aria-label={label} />
+      <img
+        onClick={() => setZoom(1)}
+        onKeyDown={onKeyDown}
+        src={src}
+        aria-label={label}
+        tabIndex={0}
+      />
       {zoom > 0 && (
         <div onClick={() => setZoom(0)} className="modal-overlay">
           <div className="modal-content">
@@ -22,9 +40,17 @@ export const Slide = ({ src, label }: SlideProps) => {
                   setZoom(2);
                   ev.stopPropagation();
                 }}
+                onKeyDown={onKeyDown}
+                tabIndex={0}
               />
             ) : (
-              <img src={src} aria-label={label} className="zoom100" />
+              <img
+                src={src}
+                aria-label={label}
+                className="zoom100"
+                onKeyDown={onKeyDown}
+                tabIndex={0}
+              />
             )}
           </div>
         </div>
